9401c25eb057ce7a9d17f6ca59cab483c5c4efd1
Frank <zhwuzhuo@gmail.com>
2017-06-10 21:43:38 +0800

    gateway

---
+++ gateway/src/main/java/spring/cloud/gateway/GatewayApplication.java
@@ -0,0 +1,19 @@
+package spring.cloud.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * Created by Frank on 2017/6/10.
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
