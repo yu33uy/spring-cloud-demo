8b148b9c1ae4af46210ea58316a6ad07763d6b9f
Frank <zhwuzhuo@gmail.com>
2017-06-10 22:35:15 +0800

    整理

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # spring-cloud-demo
+
+## hosts 
+
+```$xslt
+127.0.0.1   config
+127.0.0.1   registry
+127.0.0.1   auth-service
+127.0.0.1   gateway
+127.0.0.1   admin
+```
+
+## Config 服务
+
+地址: http://config:8888
+
+授权信息：Basic Y29uZmlnLXVzZXI6Y29uZmlnLXBhc3N3b3Jk==
--- admin/src/main/java/spring/cloud/admin/SpringBootAdminApplication.java
+++ admin/src/main/java/spring/cloud/admin/SpringBootAdminApplication.java
@@ -1,6 +1,7 @@
 package spring.cloud.admin;
 
 import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
@@ -11,4 +12,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 @EnableAdminServer
 public class SpringBootAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAdminApplication.class, args);
+    }
 }
